de45ffdd8d68e38c92d2b6df7e11baeec8b06b58
Lucas Couto <loc.unb@gmai.com>
2021-08-19 13:28:45 -0300

    criação de um serviço e um modelo para os parêmetros de URL de filtragem

--- src/app/core/filmes.service.ts
+++ src/app/core/filmes.service.ts
@@ -1,7 +1,9 @@
-import { HttpClient, HttpParams } from '@angular/common/http';
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
+import { ConfigParams } from '../shared/models/config-params';
 import { Filme } from '../shared/models/filme';
+import { ConfigParamsService } from './config-params.service';
 
 const url = 'http://localhost:3000/filmes/';
 
@@ -10,21 +12,16 @@ const url = 'http://localhost:3000/filmes/';
 })
 export class FilmesService{
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient,
+              private configParams: ConfigParamsService) { }
 
   salvar(filme: Filme): Observable<Filme> {
     return this.http.post<Filme>(url, filme);
   }
 
-  listar(pagina: number, qtdPagina: number, texto: string, genero: string): Observable<Filme[]> {
-    let httpParams = new HttpParams();
-    httpParams = httpParams.set('_page', pagina.toString());
-    httpParams = httpParams.set('_limit', qtdPagina.toString());
-    httpParams = httpParams.set('_sort', 'id');
-    httpParams = httpParams.set('_order', 'desc');
-    if (genero) httpParams = httpParams.set('genero', genero);
-    if (texto) httpParams = httpParams.set('q', texto);
-    return this.http.get<Filme[]>(url, { params: httpParams });
+  listar(config: ConfigParams): Observable<Filme[]> {
+    const configParams = this.configParams.configurarParametros(config);
+    return this.http.get<Filme[]>(url, { params: configParams });
   }
 
 }
--- src/app/filmes/listagem-filmes/listagem-filmes.component.ts
+++ src/app/filmes/listagem-filmes/listagem-filmes.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup } from '@angular/forms';
 import { FilmesService } from 'src/app/core/filmes.service';
+import { ConfigParams } from 'src/app/shared/models/config-params';
 import { Filme } from 'src/app/shared/models/filme';
 
 @Component({
@@ -10,10 +11,10 @@ import { Filme } from 'src/app/shared/models/filme';
 })
 export class ListagemFilmesComponent implements OnInit {
 
-  readonly qtdPagina = 4;
-  pagina = 0;
-  texto = "";
-  genero = "";
+  config: ConfigParams = {
+    pagina: 0,
+    limite: 4
+  }
   filmes: Filme[] = [];
   filtrosListagem: FormGroup;
   generos: Array<string>;
@@ -27,12 +28,12 @@ export class ListagemFilmesComponent implements OnInit {
     });
 
     this.filtrosListagem.get('texto').valueChanges.subscribe((val: string) => {
-      this.texto = val;
+      this.config.pesquisa = val;
       this.resetarConsulta();
     });
 
     this.filtrosListagem.get('genero').valueChanges.subscribe((val: string) => {
-      this.genero = val;
+      this.config.campo = {tipo: 'genero', valor: val};
       this.resetarConsulta();
     });
 
@@ -46,13 +47,13 @@ export class ListagemFilmesComponent implements OnInit {
   }
 
   private listarFilmes(): void {
-    this.pagina++;
-    this.filmesService.listar(this.pagina, this.qtdPagina, this.texto, this.genero)
+    this.config.pagina++;
+    this.filmesService.listar(this.config)
       .subscribe((filmes: Filme[]) => this.filmes.push(...filmes));
   }
 
   private resetarConsulta(): void {
-    this.pagina = 0;
+    this.config.pagina = 0;
     this.filmes = [];
     this.listarFilmes();
   }
